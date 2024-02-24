f3a95580081aa56c70af7b21cd4f599ba6991609
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-02-23 22:13:56 -0500

    submit assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const copyOfCatsUsingSlice = cats.slice();
+const copyOfCatsUsingSpread = [...cats];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name){
+    const copy = [...cats];
+       copy.push(name);
+       return copy;
+}
+function prependCat(name){
+    const copy = [...cats];
+       copy.unshift(name);
+       return copy;
+}
+function removeLastCat(){
+    const copy = [...cats];
+       copy.pop();
+       return copy;
+}
+function removeFirstCat(name){
+    const copy = [...cats];
+       copy.shift();
+       return copy;
+}
